4d6b0c7c739ac2871f885a6c047ad775ea17bf31
Nguyen Tien Anh <47881661+Anhgrew@users.noreply.github.com>
2022-07-17 08:08:12 +0700

    fix: handle blocked issue by CORS policy

--- app/main.py
+++ app/main.py
@@ -1,15 +1,32 @@
-# from functools import lru_cache
 from fastapi import FastAPI
 from core.config import settings
 from db.db import initiate_database
 from routes.user import router as UserRouter
 
+
+# Create new fastapi app instance
 app = FastAPI()
 
 
+# A list of origins that should be permitted to make cross-origin requests
+origins = ["*"]
+
+# Add middleware to app for CORS (Cross-Origin Resource Sharing)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    #  Indicate that cookies should be supported for cross-origin requests.
+    allow_credentials=True,
+    # A list of HTTP methods that should be allowed for cross-origin requests
+    allow_methods=["*"],
+    # A list of HTTP request headers that should be supported for cross-origin requests
+    allow_headers=["*"],
+)
+
+# Include startup event
 @app.on_event("startup")
 async def start_database():
     await initiate_database()
 
-
+# Include router of User NFT Item
 app.include_router(UserRouter, prefix="/users")
